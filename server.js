4f34557cb937f5c1070ec9ab7661f300a2ac9924
Twigster <treshagaye@gmail.com>
2022-03-03 20:48:54 -0500

    created draft html for handlebars loading

--- server.js
+++ server.js
@@ -1,4 +1,44 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const helpers = require('./utils/helpers');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Mystery user',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
+
+// turn on routes
+app.use(routes);
+
+// set on handlebars as template engine
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
      
  
  
