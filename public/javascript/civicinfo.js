7658f5ef5642eefce0800f35c84c9d3dc43b4ce6
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-03-07 12:24:47 -0500

    frontend updates

--- public/javascript/civicinfo.js
+++ public/javascript/civicinfo.js
@@ -1,11 +1,14 @@
-var apiKey = "AIzaSyDE4huvmH6VOPODWGRYgvl_umZVcfWHB_s";
-var userAddress = "06095";
-var civicContainer = document.getElementById('civic-rep');
 var submitBtn =document.getElementById('.submit-btn')
 
 console.log("connect to html")
 function civicInfo() {
-  fetch("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=" + userAddress + "&roles=legislatorLowerBody&key="+ apiKey)
+  const apiKey = "AIzaSyDE4huvmH6VOPODWGRYgvl_umZVcfWHB_s";
+  const userAddress = "06095";
+  const civicContainer = document.getElementById('civic-rep');
+
+  // fetch("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address="+ userAddress + "&includeOffices=true&roles=headOfState&roles=headOfGovernment&roles=deputyHeadOfGovernment&roles=governmentOfficer&roles=executiveCouncil&roles=legislatorUpperBody&roles=legislatorLowerBody&key="+ apiKey)
+
+  fetch("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address="+ userAddress +" &includeOffices=true&roles=headOfState&roles=headOfGovernment&roles=deputyHeadOfGovernment&roles=governmentOfficer&roles=executiveCouncil&roles=legislatorUpperBody&roles=legislatorLowerBody&roles=highestCourtJudge&roles=judge&roles=schoolBoard&roles=specialPurposeOfficer&key="+ apiKey)
   .then(response => {
     console.log(response);
     return response.json();
@@ -15,16 +18,48 @@ function civicInfo() {
     console.log(data.officials[0].name)
     console.log(data.officials[0].party)
 
-    var repNameEl = document.createElement("h6");
-    repNameEl.textContent = "Your Representative is: " + data.officials[0].name;
-    repNameEl.classList.add("is-size-5", "has-text-weight-bold");
+    const repNameEl = document.createElement("h6");
+    repNameEl.textContent = "Your " + data.normalizedInput.state + " Representatives are: " + data.officials[2].name + " * " + data.officials[3].name + " * " + data.officials[4].name + " * " + data.officials[5].name + " * " + data.officials[6].name + " * " + data.officials[7].name + " * " + data.officials[8].name + " * " + data.officials[9].name + " * " + data.officials[10].name;
+    // repNameEl.classList.add("is-size-5", "has-text-weight-bold");
     civicContainer.appendChild(repNameEl);
 
-    var repPartyEl = document.createElement("h6");
+    const repPartyEl = document.createElement("h6");
     repPartyEl.textContent =  data.officials[0].party;
-    repPartyEl.classList.add("is-size-5", "has-text-weight-bold");
+    // repPartyEl.classList.add("is-size-5", "has-text-weight-bold");
     civicContainer.appendChild(repPartyEl);
   });
 }
 
 civicInfo();
+
+// async function civicInfo(event) {
+//   event.preventDefault();
+
+//   const apiKey = "AIzaSyDE4huvmH6VOPODWGRYgvl_umZVcfWHB_s";
+//   const civicContainer = document.getElementById('civic-rep');
+
+//   console.log('button clicked');
+//   fetch("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=" + userAddress + "legislatorLowerBody&key="+ apiKey)
+//   .then(response => {
+//     console.log(response);
+//     return response.json();
+//   })
+//   . then(data => {
+//     console.log(data)
+//     console.log(data.officials[0].name)
+//     console.log(data.officials[0].party)
+
+//     const repNameEl = document.createElement("h6");
+//     repNameEl.textContent = "Your Representative is: " + data.officials[0].name;
+//     repNameEl.classList.add("is-size-5", "has-text-weight-bold");
+//     civicContainer.appendChild(repNameEl);
+
+//     const repPartyEl = document.createElement("h6");
+//     repPartyEl.textContent =  data.officials[0].party;
+//     repPartyEl.classList.add("is-size-5", "has-text-weight-bold");
+//     civicContainer.appendChild(repPartyEl);
+//   });
+// }
+
+// document.querySelector('.submit-btn').addEventListener('click', civicInfo);
+
