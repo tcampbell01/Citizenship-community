a562c4f7f8184c6be1822f793e0ff94e4b65987b
Joseph <92699311+JMCampbell2021@users.noreply.github.com>
2022-03-12 20:33:49 -0500

    refactor civic api and deleted html files

--- public/javascript/civicinfo.js
+++ public/javascript/civicinfo.js
@@ -1,28 +1,18 @@
-var submitBtn =document.getElementById('.submit-btn')
-
-console.log("connect to html")
 function civicInfo() {
   const apiKey = "AIzaSyDE4huvmH6VOPODWGRYgvl_umZVcfWHB_s";
   var userAddress = document.getElementById('legislatorsVal');
   userAddress = userAddress.innerHTML; 
-  // const userAddress = "06095";
   const civicContainer = document.getElementById('civic-rep');
+  const url = "https://civicinfo.googleapis.com/civicinfo/v2/representatives?address=" + userAddress + "&includeOffices=true&roles=headOfState&roles=headOfGovernment&roles=deputyHeadOfGovernment&roles=governmentOfficer&roles=executiveCouncil&roles=legislatorUpperBody&roles=legislatorLowerBody&key="+ apiKey;
 
-  fetch("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address="+ userAddress + "&includeOffices=true&roles=headOfState&roles=headOfGovernment&roles=deputyHeadOfGovernment&roles=governmentOfficer&roles=executiveCouncil&roles=legislatorUpperBody&roles=legislatorLowerBody&key="+ apiKey)
-
-  // fetch("https://civicinfo.googleapis.com/civicinfo/v2/representatives?address="+ userAddress +" &includeOffices=true&roles=headOfState&roles=headOfGovernment&roles=deputyHeadOfGovernment&roles=governmentOfficer&roles=executiveCouncil&roles=legislatorUpperBody&roles=legislatorLowerBody&roles=highestCourtJudge&roles=judge&roles=schoolBoard&roles=specialPurposeOfficer&key="+ apiKey)
+  fetch(url)
   .then(response => {
     console.log(response);
     return response.json();
   })
   . then(data => {
     console.log(data)
-    console.log(data.officials[0].name)
-    console.log(data.officials[0].party)
-    console.log(data.offices[2].name)
-    console.log(data.offices[3].name)
-    console.log(data.offices[4].name)
-
+ 
     const senatorEl = document.createElement("h6");
     senatorEl.textContent = "Your Senators = " + data.officials[2].name + " and " + data.officials[3].name; 
     const usRepEl = document.createElement("h6");
@@ -38,4 +28,3 @@ function civicInfo() {
 }
 
 civicInfo();
-
